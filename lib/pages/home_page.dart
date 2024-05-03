bb55b6b6fab0fdf3c301200289297fa174e09f97
minhmoidz <minhtuantran210305@gmail.com>
2024-05-03 16:44:50 +0700

    03052004

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,188 @@
 import 'package:flutter/material.dart';
 import 'package:fluttericon/mfg_labs_icons.dart';
 import 'package:aldabot/pages/chat_page.dart';
+import 'package:aldabot/pages/voice_page.dart';
+import 'package:aldabot/pages/home_page.dart'; // Import trang HomePage
+
+void main() {
+  runApp(LoginApp());
+}
+
+class LoginApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: TextTheme(
+          subtitle1: TextStyle(color: Colors.white),
+        ),
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _login() {
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    // Kiểm tra tên đăng nhập và mật khẩu cố định
+    if (username == 'hung123' && password == '12345') {
+      print('Đăng nhập thành công!');
+      // Thực hiện hành động khi đăng nhập thành công, chuyển đến trang chính của ứng dụng
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    } else {
+      print('Tên đăng nhập hoặc mật khẩu không đúng. Vui lòng thử lại.');
+      // Hiển thị thông báo lỗi cho người dùng
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: Image.asset(
+                  'images/icon.png',
+                  width: 205,
+                  height: 205,
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: _usernameController,
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    labelStyle: TextStyle(color: Colors.black),
+                    prefixIcon: Icon(Icons.person, color: Colors.black),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: _passwordController,
+                  style: TextStyle(color: Colors.black),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.black),
+                    prefixIcon: Icon(Icons.lock, color: Colors.black),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _login,
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  //primary : Colors.blue, // Màu nút khi không kích hoạt
+                  elevation: 15, // Độ nổi của nút
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 48.0),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 57, 123, 166),
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      // Hành động khi nhấn vào tạo tài khoản
+                    },
+                    child: Text(
+                      'Create Account',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    ' | ',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // Hành động khi nhấn vào quên mật khẩu
+                    },
+                    child: Text(
+                      'Forgot Password',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class HomePage extends StatelessWidget {
   final double horizontalPadding = 20;
@@ -12,12 +194,12 @@ class HomePage extends StatelessWidget {
       "Chatting",
       MfgLabs.comment,
       Color.fromARGB(255, 133, 177, 213),
-    ], // Sử dụng MfgLabs.comment thay vì Icon
+    ],
     [
       "Talking",
       MfgLabs.mic,
       Color.fromARGB(255, 102, 162, 104),
-    ], // Sử dụng MfgLabs.mic thay vì Icon
+    ],
   ];
 
   @override
@@ -34,16 +216,27 @@ class HomePage extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Icon(MfgLabs.menu,
-                      size: 36,
-                      color: Colors.black54), // Thay đổi màu icon menu
-                  Icon(Icons.person,
-                      size: 36,
-                      color: Colors.black54), // Thay đổi màu icon person
+                  IconButton(
+                    icon: Icon(Icons.logout), // Biểu tượng nút thoát
+                    onPressed: () {
+                      // Xử lý sự kiện khi nút được nhấn
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                LoginPage()), // Điều hướng đến trang đăng nhập
+                      );
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.settings), // Thay đổi icon thành settings
+                    onPressed: () {
+                      // Thực hiện hành động khi icon được nhấn
+                    },
+                  ),
                 ],
               ),
             ),
-
             SizedBox(height: heightSizeBox),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
@@ -54,15 +247,13 @@ class HomePage extends StatelessWidget {
                       style: TextStyle(
                           fontSize: 28,
                           fontWeight: FontWeight.bold,
-                          color: Colors
-                              .black)), // Thay đổi kích thước và font-weight của tiêu đề chào mừng
+                          color: Colors.black)),
                   Text("HUNG MANH",
                       style: TextStyle(
                           fontSize: 30,
                           fontWeight: FontWeight.bold,
                           fontFamily: 'Roboto',
-                          color: Color.fromARGB(
-                              255, 101, 151, 170))), // Thay đổi màu chữ
+                          color: Color.fromARGB(255, 101, 151, 170))),
                 ],
               ),
             ),
@@ -73,10 +264,9 @@ class HomePage extends StatelessWidget {
                   style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
-                      color: Colors
-                          .black87)), // Thay đổi font-size, font-weight và màu chữ
+                      color: Colors.black87)),
             ),
-            SizedBox(height: 30), // Giảm khoảng cách giữa dòng văn bản và lưới
+            SizedBox(height: 30),
             Column(
               children: [
                 GestureDetector(
@@ -130,7 +320,7 @@ class HomePage extends StatelessWidget {
                   onTap: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => ChatPage()),
+                      MaterialPageRoute(builder: (context) => VoicePage()),
                     );
                   },
                   child: Container(
@@ -175,10 +365,10 @@ class HomePage extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: heightSizeBox), // Thêm khoảng cách dưới cùng
+            SizedBox(height: heightSizeBox),
             Expanded(
-                child:
-                    Container()), // Expanded để đẩy slogan xuống dưới cuối màn hình
+              child: Container(),
+            ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
               child: Container(
@@ -222,7 +412,7 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(height: heightSizeBox), // Thêm khoảng cách dưới cùng
+            SizedBox(height: heightSizeBox),
           ],
         ),
       ),
---
+++ lib/pages/voice_page.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:permission_handler/permission_handler.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'dart:convert';
+
+class VoicePage extends StatefulWidget {
+  const VoicePage({Key? key}) : super(key: key);
+
+  @override
+  State<VoicePage> createState() => _VoicePageState();
+}
+
+class _VoicePageState extends State<VoicePage> {
+  final stt.SpeechToText _speech = stt.SpeechToText();
+  bool _isRecording = false;
+  String _text = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 36, 185, 138),
+        title: const Text(
+          'ALDA Voice',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // Phần hiển thị câu trả lời
+          Expanded(
+            child: Center(
+              child: Text(
+                _text,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+          // Phần chứa biểu tượng mic và nút gửi tin nhắn
+          Container(
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  onPressed: _toggleRecording,
+                  icon: Icon(
+                    _isRecording ? Icons.stop : Icons.mic,
+                    size: 100,
+                    color: _isRecording ? Colors.red : Colors.blue,
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _sendText,
+                  child: Text('Send Text'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _toggleRecording() async {
+    if (!_isRecording) {
+      await _startRecording();
+    } else {
+      await _stopRecording();
+    }
+  }
+
+  Future<void> _startRecording() async {
+    if (await _checkPermission()) {
+      setState(() {
+        _isRecording = true;
+        _text = 'Listening...';
+      });
+
+      _speech.listen(onResult: (SpeechRecognitionResult result) {
+        setState(() {
+          _text = result.recognizedWords;
+        });
+      });
+    }
+  }
+
+  Future<void> _stopRecording() async {
+    setState(() {
+      _isRecording = false;
+      _text = 'Processing...';
+    });
+
+    _speech.stop();
+  }
+
+  Future<bool> _checkPermission() async {
+    var microphoneStatus = await Permission.microphone.request();
+    return microphoneStatus.isGranted;
+  }
+
+  Future<void> _sendText() async {
+    if (_text.isNotEmpty) {
+      final url = 'http://203.162.88.102:12001/chat/';
+      final data = {
+        "id_user": 1,
+        "id_section": 2,
+        "query": _text,
+      };
+
+      try {
+        final response = await http.post(
+          Uri.parse(url),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: jsonEncode(data),
+        );
+
+        if (response.statusCode == 200) {
+          var responseData = jsonDecode(utf8.decode(response.bodyBytes));
+          String botReply = 'Failed to get response from ALDA bot';
+          print(responseData);
+          if (responseData['response'] != null) {
+            botReply = responseData['response'];
+          }
+          setState(() {
+            _text = botReply;
+          });
+        } else {
+          print('Request failed with status: ${response.statusCode}.');
+          print('Response body: ${response.body}');
+        }
+      } catch (e) {
+        print('Error: $e');
+      }
+    }
+  }
+}
